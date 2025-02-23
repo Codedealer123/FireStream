4960054621be159336b86356f09d6718d93e86f0
DustySoul1 <mukilan.2.2014@gmail.com>
2025-02-23 09:40:17 -0600

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Firebase Configuration (Replace with your project details)
+const firebaseConfig = {
+    apiKey: "AIzaSyDwvLDYDMBRZFW3RO3CnvLuDXPiRnR7Hes",
+    authDomain: "firestream-codedealer123.firebaseapp.com",
+    projectId: "firestream-codedealer123",
+    storageBucket: "firestream-codedealer123.firebasestorage.app",
+    messagingSenderId: "992477689322",
+    appId: "1:992477689322:web:8856f9f98d28e32ef72e93"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const provider = new firebase.auth.GoogleAuthProvider();
