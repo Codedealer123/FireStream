00095dd316b67357420f31654a4cbd203d15b5c8
DustySoul1 <mukilan.2.2014@gmail.com>
2025-02-23 09:58:40 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,3 +1,6 @@
+import firebase from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import firebaseAuth from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import FirebaseFirestore from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 // Firebase Configuration (Replace with your project details)
 const firebaseConfig = {
     apiKey: "AIzaSyDwvLDYDMBRZFW3RO3CnvLuDXPiRnR7Hes",
